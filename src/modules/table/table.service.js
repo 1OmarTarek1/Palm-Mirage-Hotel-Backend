9faa0305ef3e1b6b616a5cb6a587abd0756a7c4d
AmeraaGamal <ameragamal425@gmail.com>
2026-03-23 15:36:17 +0200

    create table

--- src/modules/table/table.service.js
+++ src/modules/table/table.service.js
@@ -2,3 +2,16 @@ import Table from '../../DB/Model/table.model.js';
 import { asyncHandler } from '../../../src/utils/response/error.response.js';
 import * as dbService from '../../../DB/db.service.js';
 
+export const createTable = asyncHandler(async (req, res) => {
+  const { number, chairs } = req.body;
+
+  try {
+    const table = await dbService.create({ model: Table, data: { number, chairs } });
+    res.status(201).json({ message: 'Table created', table });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(409).json({ message: 'Table number already exists' });
+    }
+    throw error;
+  }
+});
