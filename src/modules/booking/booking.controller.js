b1afec338ba89e464513997ee20e3dd8551005b1
AmeraaGamal <ameragamal425@gmail.com>
2026-03-23 16:10:31 +0200

    Add booking routes: create booking, get available tables, cancel booking

---
+++ src/modules/booking/booking.controller.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import {
+  createBooking,
+  getAvailableTables,
+  cancelBooking,
+} from './services/booking.service.js';
+import { validation } from '../../middleware/validation.middleware.js';
+import { authentication } from '../../middleware/auth.middleware.js';
+import * as validators from './booking.validation.js';
+
+const router = Router();
+
+router.post(
+  '/book',
+  authentication(),
+  validation(validators.createBooking),
+  createBooking
+);
+
+router.get(
+  '/available-tables',
+  validation(validators.getAvailableTables),
+  getAvailableTables
+);
+router.patch('/cancel/:id', authentication(), cancelBooking);
+export default router;
