e54cebc2a1445ef4b8456a60e6b7b178decd65fb
AmeraaGamal <ameragamal425@gmail.com>
2026-03-24 20:01:33 +0200

    fix: resolve conflict issue

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-import connectDB from "./DB/conenction.js";
-import cors from "cors";
-import authController from "./modules/auth/auth.controller.js";
-import userController from "./modules/user/user.controller.js";
-import activityController from "./modules/activity/activity.controller.js";
-import roomController from "./modules/rooms/room.controller.js";
-import paymentController from "./modules/payment/payment.controller.js";
-import { globalErrorHandling } from "./utils/response/error.response.js";
-import helmet from "helmet";
-import rateLimit from "express-rate-limit";
-=======
 import connectDB from './DB/conenction.js';
+import cors from 'cors';
 import authController from './modules/auth/auth.controller.js';
 import userController from './modules/user/user.controller.js';
-import bookingController from './modules/bookingTable/booking.controller.js';
-import tableController from './modules/restaurantTable/restaurantTable.controller.js';
+import activityController from './modules/activity/activity.controller.js';
+import roomController from './modules/rooms/room.controller.js';
+import paymentController from './modules/payment/payment.controller.js';
 import { globalErrorHandling } from './utils/response/error.response.js';
 import helmet from 'helmet';
-import rateLimit from 'express-rate-limit';
->>>>>>> a0f43cc (Add Table model)
+// import rateLimit from "express-rate-limit";
+import bookingController from './modules/bookingTable/booking.controller.js';
+import tableController from './modules/restaurantTable/restaurantTable.controller.js';
 
 // const limiter = rateLimit({
 //   limit: 5,
@@ -28,7 +19,7 @@ import rateLimit from 'express-rate-limit';
 
 const bootstrap = (app, express) => {
   // Allow frontend origins (from .env.dev ORAGIN) to call our API and send cookies
-  const whitelist = process.env.ORAGIN?.split(",").map((o) => o.trim()) || [];
+  const whitelist = process.env.ORAGIN?.split(',').map((o) => o.trim()) || [];
 
   // Allow frontend origins (from .env.dev ORAGIN) to call our API and send cookies
   app.use(
@@ -43,16 +34,16 @@ const bootstrap = (app, express) => {
 
   app.use(express.json());
   app.use(helmet());
-  app.get("/", (req, res) => res.send({ message: "Hello World!" }));
-  app.use("/auth", authController);
-  app.use("/user", userController);
-  app.use("/activity", activityController);
-  app.use("/room", roomController);
-  app.use("/payment", paymentController);
+  app.get('/', (req, res) => res.send({ message: 'Hello World!' }));
+  app.use('/auth', authController);
+  app.use('/user', userController);
+  app.use('/activity', activityController);
+  app.use('/room', roomController);
+  app.use('/payment', paymentController);
   app.use('/booking', bookingController);
   app.use('/tables', tableController);
-  app.use("*", (req, res, next) => {
-    return res.status(404).json({ message: "Invalid routing" });
+  app.use('*', (req, res, next) => {
+    return res.status(404).json({ message: 'Invalid routing' });
   });
 
   app.use(globalErrorHandling);
