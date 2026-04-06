f2ce75d5c67062fc737cec329c96b7cbcfaa2119
1OmarTarek1 <cr7omartarek@gmail.com>
2026-04-06 13:54:50 +0200

    prepare backend for production (start script + cron)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "./src/utils/deleteExpierOTP/cron.js";
 import path from 'node:path'
 import http from 'node:http'
 import * as dotenv from 'dotenv'
@@ -5,7 +6,6 @@ dotenv.config({ path: path.resolve("./src/config/.env.dev") })
 import bootstrap from './src/app.controller.js'
 import express from 'express'
 import { initializeSocket } from './src/socket/index.js'
-import "./src/utils/deleteExpierOTP/cron.js"; 
 const app = express()
 const port = process.env.PORT || 5000
 
